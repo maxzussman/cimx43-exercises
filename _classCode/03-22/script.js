b8327735df1871fdd094f0726ecbb0347bdf1e3a
Max Zuss <maxzussman@gmail.com>
2021-03-23 17:36:20 -0400

    updatesr

--- _classCode/03-22/script.js
+++ _classCode/03-22/script.js
@@ -1,1 +1,17 @@
 console.log('wip');
+
+let newLink = document.createElement('a');
+let allParagraphs = document.getElementsByTagName('p');
+// allParagraphs is 'list' of every p tag on page
+const firstParagraph = allParagraphs[0];
+
+//firstParagraph gets first item. in JS first item is positioned at 0
+console.log('allParagraphs');
+
+newLink.setAttribute('href', 'https//:google.com');
+
+newLink.style.display = 'inner-block';
+
+newLink.innerHTML = 'take me to google';
+
+firstParagraph.appendChild(newLink);
